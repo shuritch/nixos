74adb1824c95111a5147a3b3ecca380bd8604108
Alexander Ivanov <sashapop10@yandex.ru>
2024-09-22 00:24:53 +0300

    fix(shell/fish): Aliasses & Abbreviations

--- library/modules/home/shellopts.nix
+++ library/modules/home/shellopts.nix
@@ -5,51 +5,13 @@ in {
     aliases = lib.mkOption {
       type = lib.types.attrsOf lib.types.str;
       description = "Shell abbreviations";
-      default = {
-        # Clear screen and scrollback
-        c = "printf '\\033[2J\\033[3J\\033[1;1H'";
-        clear = "printf '\\033[2J\\033[3J\\033[1;1H'";
-      };
+      default = { };
     };
 
-    abbrs = let
-      inherit (lib) mkIf;
-      packageNames = map (p: p.pname or p.name or null) config.home.packages;
-      hasPackage = name: lib.any (x: x == name) packageNames;
-      hasSpecialisationCli = hasPackage "specialisation";
-      hasKitty = config.programs.kitty.enable;
-      hasEza = hasPackage "eza";
-    in lib.mkOption {
+    abbrs = lib.mkOption {
       type = lib.types.attrsOf lib.types.str;
       description = "Shell abbreviations";
-      default = {
-        nix-rebuild = "sudo nixos-rebuild --flake $FLAKE";
-        nix-switch = "sudo nixos-rebuild --flake $FLAKE switch";
-        nix-clean = "sudo nix-collect-garbage -d";
-        nix-test = "sudo nixos-rebuild --flake $FLAKE test";
-        home-rebuild = "home-manager --flake $FLAKE";
-        home-switch = "home-manager --flake $FLAKE switch";
-        nix-edit = "code $FLAKE";
-
-        v = "nvim";
-        vim = "nvim";
-        js = "node";
-        dsize = "du -hs";
-
-        htop = mkIf (hasPackage "btop") "btop";
-        cat = mkIf (hasPackage "bat") "bat";
-        space = mkIf (hasPackage "ncdu") "ncdu";
-        cd = mkIf (hasPackage "zoxide") "z";
-        s = mkIf hasSpecialisationCli "specialisation";
-        cik = mkIf hasKitty "clone-in-kitty --type os-window";
-
-        ls = mkIf hasEza "eza";
-        exa = mkIf hasEza "eza";
-        l = mkIf hasEza "eza --icons  -a --group-directories-first -1";
-        tree = mkIf hasEza "eza --icons --tree --group-directories-first";
-        ll = mkIf hasEza
-          "eza --icons  -a --group-directories-first -1 --no-user --long";
-      };
+      default = { };
     };
 
     enableBashIntegration = lib.mkOption {
@@ -104,5 +66,47 @@ in {
 
   config.programs.bash = lib.mkIf cfg.enableBashIntegration {
     shellAliases = cfg.aliases // cfg.abbrs;
+  };
+
+  config.shell.aliases = {
+    # Clear screen and scrollback
+    c = "printf '\\033[2J\\033[3J\\033[1;1H'";
+    clear = "printf '\\033[2J\\033[3J\\033[1;1H'";
+  };
+
+  config.shell.abbrs = let
+    inherit (lib) mkIf;
+    packageNames = map (p: p.pname or p.name or null) config.home.packages;
+    hasPackage = name: lib.any (x: x == name) packageNames;
+    hasSpecialisationCli = hasPackage "specialisation";
+    hasKitty = config.programs.kitty.enable;
+    hasEza = hasPackage "eza";
+  in {
+    nix-rebuild = "sudo nixos-rebuild --flake $FLAKE";
+    nix-switch = "sudo nixos-rebuild --flake $FLAKE switch";
+    nix-clean = "sudo nix-collect-garbage -d";
+    nix-test = "sudo nixos-rebuild --flake $FLAKE test";
+    home-rebuild = "home-manager --flake $FLAKE";
+    home-switch = "home-manager --flake $FLAKE switch";
+    nix-edit = "code $FLAKE";
+
+    v = "nvim";
+    vim = "nvim";
+    js = "node";
+    dsize = "du -hs";
+
+    htop = mkIf (hasPackage "btop") "btop";
+    cat = mkIf (hasPackage "bat") "bat";
+    space = mkIf (hasPackage "ncdu") "ncdu";
+    cd = mkIf (hasPackage "zoxide") "z";
+    s = mkIf hasSpecialisationCli "specialisation";
+    cik = mkIf hasKitty "clone-in-kitty --type os-window";
+
+    ls = mkIf hasEza "eza";
+    exa = mkIf hasEza "eza";
+    l = mkIf hasEza "eza --icons  -a --group-directories-first -1";
+    tree = mkIf hasEza "eza --icons --tree --group-directories-first";
+    ll = mkIf hasEza
+      "eza --icons  -a --group-directories-first -1 --no-user --long";
   };
 }
