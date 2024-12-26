4f6415431fc2650d442370cfa9dcb60076df1a0d
Alexander Ivanov <sashapop10@yandex.ru>
2024-12-26 04:16:39 +0300

    🔨 Resolved conflict with pandora

--- cluster/pandora/configuration.nix
+++ cluster/pandora/configuration.nix
@@ -1,4 +1,5 @@
 { lib, ... }: {
+  services.openssh.settings.PermitRootLogin = "yes";
   programs.ssh.setXAuthLocation = lib.mkForce false;
   my.security.wheelNeedsPassword = false;
   my.network.wirelessBackend = "iwd";
