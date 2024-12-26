3ebc993b70d0c586e65f5c27946f7635d34314d3
Alexander Ivanov <sashapop10@yandex.ru>
2024-12-26 03:59:21 +0300

    🔌 SSH Blocks

--- src/modules/home/global/ssh.nix
+++ src/modules/home/global/ssh.nix
@@ -3,6 +3,22 @@
     enable = true;
     hashKnownHosts = true;
     compression = true;
-    matchBlocks = { };
+    matchBlocks = {
+      home-vpn = {
+        host = "194.0.194.170";
+        identitiesOnly = true;
+        identityFile = "~/.ssh/id_ed25519";
+        user = "shuritch";
+        port = 49152;
+      };
+
+      uberi-ssh = {
+        host = "78.140.220.18";
+        identitiesOnly = true;
+        identityFile = "~/.ssh/uberi/id_rsa";
+        user = "wwwadm";
+        port = 22;
+      };
+    };
   };
 }
