9b8cea20657abe811c3ca248238d35be79a0d3b8
Alexander Ivanov <sashapop10@yandex.ru>
2025-01-24 19:24:57 +0300

    🔑 VPN-Zapret Toggle

--- src/modules/home/programs/other/nekoray.nix
+++ src/modules/home/programs/other/nekoray.nix
@@ -4,6 +4,24 @@ let cfg = config.my.home;
 in {
   options.my.home.programs.nekoray.enable = lib.mkEnableOption "Enable nekoray";
   config = lib.mkIf (cfg.desktop.enable && cfg.programs.nekoray.enable) {
-    home.packages = with pkgs; [ nekoray ];
+    home.packages = with pkgs;
+      [ nekoray ] ++ (lib.optionals config.my.network.frkn.enable [
+        (pkgs.writeShellApplication {
+          runtimeInputs = with pkgs; [ systemd nekoray ];
+          name = "toggle-vpn";
+          text = ''
+            #!/usr/bin/env bash
+            status=$(systemctl is-active zapret.service)
+            export SHELL="/run/current-system/sw/bin/fish"
+            if [[ "$status" == "active" ]]; then
+                systemctl stop zapret.service
+                nekoray
+            else
+                systemctl start zapret.service
+                pkill nekoray
+            fi
+          '';
+        })
+      ]);
   };
 }
